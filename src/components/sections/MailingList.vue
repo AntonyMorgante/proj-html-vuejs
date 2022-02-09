d0b931d8d0338c05618b0b4f7a86abc350d1d667
AntonyMorgante <antonymorgante1@gmail.com>
2022-02-09 16:50:58 +0100

    improved shadow on mailing section

--- src/components/sections/MailingList.vue
+++ src/components/sections/MailingList.vue
@@ -42,7 +42,7 @@ export default {
     background-image: url("../../assets//banner1-2x.jpg");
     background-size: cover;
     min-height: 650px;
-    box-shadow: inset 900px 0px 60px -10px $dark;
+    box-shadow: inset 1100px 0px 200px -10px $dark;
   }
   
   .fs-22{
