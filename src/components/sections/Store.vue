16084a2749204a7c5cd11dd1c9f394a6fc8e98aa
AntonyMorgante <antonymorgante1@gmail.com>
2022-02-10 10:45:44 +0100

    store section

--- src/components/sections/Store.vue
+++ src/components/sections/Store.vue
@@ -1,15 +1,57 @@
 <template>
   <section id="store">
-
+    <div class="container text-center">
+      <div class="content-container">
+        <p class="title">
+          "I've failed over and over again in my life. And that is why I succeed."
+        </p>
+        <Waves />
+        <p class="paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius repudiandae omnis ratione aliquid, dolores dicta assumenda maiores aliquam?
+        </p>
+        <button class="blue button">Buy Avada today <i class="fa-solid fa-angle-right"></i></button>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import Waves from '../commons/Waves.vue'
 export default {
+  components: { Waves },
     name: "Store"
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../assets/globals.scss";
 
+  #store{
+    padding:100px;
+    background-image: url("../../assets/banner2-2x.jpg");
+    background-size: cover;
+    min-height: 550px;
+    box-shadow: inset 0 -150px 500px 100px rgb(2, 2, 14);
+    color:white;
+  }
+
+  .content-container{
+    width:75%;
+    margin:0 auto;
+  }
+
+  .title{
+    font-size:60px;
+    margin: 80px 0 40px;
+  }
+
+  .paragraph{
+    font-size: 25px;
+    margin: 15px 0 35px;
+  }
+
+  .blue.button{
+    font-size:16px;
+    padding: 18px 40px;
+  }
 </style>
