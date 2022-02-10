6bd0279db7abe35d07127bccb8618709e9ba62ce
AntonyMorgante <antonymorgante1@gmail.com>
2022-02-10 11:16:22 +0100

    partners section

---
+++ src/components/commons/Partner.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="partner-card text-center">
+    <img :src="getUrl(img)" alt="">
+    <Waves />
+    <p class="paragraph">{{description}}</p>
+    <a class="text-black redirect" :href="redirection">{{disclaimer}} <i class="fa-solid fa-angle-right"></i></a>
+  </div>
+</template>
+
+<script>
+import Waves from "../commons/Waves.vue"
+
+export default {
+    name:"Partner",
+    props:{
+      img:String,
+      description:String,
+      redirection:String,
+      disclaimer:String
+    },
+    components:{
+      Waves
+    },
+    methods:{
+      getUrl(url){
+        return require("../../assets/" + url);
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/globals.scss";
+
+  .partner-card{
+    flex-basis:20%;
+
+    img{
+      width:60%;
+      margin-bottom: 15px;
+    }
+  }
+
+  .waves{
+    color:$primary;
+    font-size:20px;
+  }
+  
+  .paragraph{
+    font-size:18px;
+    line-height: 25px;
+    margin-bottom: 40px;
+  }
+
+  .redirect{
+    font-size: 18px;
+  }
+</style>
--- src/components/sections/Partners.vue
+++ src/components/sections/Partners.vue
@@ -1,15 +1,67 @@
 <template>
   <section id="partners">
-
+    <div class="container">
+      <div class="row space-between">
+        <Partner
+        v-for="(partner,index) in partners"
+        :key=index
+        :img=partner.img
+        :description=partner.description
+        :redirection=partner.redirection
+        :disclaimer=partner.disclaimer
+        />
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import Partner from "../commons/Partner.vue"
+
 export default {
-    name:"Partners"
+    name:"Partners",
+    components:{
+      Partner
+    },
+    data(){
+      return{
+        partners: [
+          {
+            name:"Yoga Studio",
+            img: "sponsor-1-2x.png",
+            description:"Duis ut hendrerit mauris. Nulla eget ultrices diam. Ut mattis placerat erat et cursus.",
+            disclaimer:"Visit Yoga Studio",
+            redirection:"#"
+          },
+          {
+            name:"Fitness Center",
+            img: "sponsor-2-2x.png",
+            description:"Cras vehicula porta mattis. Curabitur tortor nulla, fermentum nec nulla et, sollicitudin tempus.",
+            disclaimer:"Join fitness center",
+            redirection:"#"
+          },
+          {
+            name:"Fitness Club",
+            img: "sponsor-3-2x.png",
+            description:"Etiam ullamcorper, velit ut laoreet, purus lorem euismod odio, justo risus porttitor urna.",
+            disclaimer:"Learn about FC",
+            redirection:"#"
+          },
+          {
+            name:"Power Gym",
+            img: "sponsor-4-2x.png",
+            description:"Proin massa purus, pulvinar in nisl eu, tincidunt bibendum libero. Consectetur, sapien a tristique.",
+            disclaimer:"Visit Power Gym",
+            redirection:"#"
+          }
+        ]
+      }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  #partners{
+    padding:100px;
+  }
 </style>
